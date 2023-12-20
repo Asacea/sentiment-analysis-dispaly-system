89e63833891fd30ee0c5bd93fd600f4766b68a99
fabulous <2030928717@qq.com>
2023-12-20 16:23:04 +0800

    啊啊啊啊

--- vite.config.js
+++ vite.config.js
@@ -27,8 +27,9 @@ export default defineConfig({
     proxy:{
       '/api':{//获取路径中包含了/api的请求
           // 后台服务所有路径
-          target:'http://localhost:8080',
-          // target:'http://47.113.190.225:8080',
+          // target:'http://localhost:8080',
+          // http://8.137.34.8/api
+          target:'http://8.137.34.8/api',
           changeOrigin:true,
           // rewrite:(path)=>path.replace(/^\/api/,'')
           rewrite:(path)=>path.replace(/^\/api/,'')
